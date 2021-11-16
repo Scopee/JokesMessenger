9dfd002a243754b93b985d6b6d412fcc1597adf1
Daniil Tolstoukhov <daniiltolstoukhov@JazzMutant.local>
2021-11-16 21:13:19 +0500

    Added jokes to messages

--- messenger/src/main/java/ru/pinguin/jokesmessenger/messages/MessagesService.java
+++ messenger/src/main/java/ru/pinguin/jokesmessenger/messages/MessagesService.java
@@ -2,17 +2,27 @@ package ru.pinguin.jokesmessenger.messages;
 
 import liquibase.pro.packaged.U;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.SpringVersion;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.pinguin.jokesmessenger.data.Message;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Service
-@RequiredArgsConstructor
 public class MessagesService {
     private final MessagesRepository messagesRepository;
+    private final Random random;
+
+    public MessagesService(MessagesRepository messagesRepository,@Value("${jokes.threshold}") Integer threshold) {
+        this.messagesRepository = messagesRepository;
+        this.threshold = threshold;
+        random = new Random();
+    }
+
+    private final Integer threshold;
 
     public List<GetMessageRequest> getMessages(UUID userId, int offset, int limit) {
         return messagesRepository.getMessages(userId, offset, limit).stream().map(this::convertToRequest).toList();
@@ -22,8 +32,38 @@ public class MessagesService {
         messagesRepository.markAsRead(messageId);
     }
 
+    @Transactional
     public void sendMessage(UUID userId, SendMessageRequest message) {
         messagesRepository.persist(convertToMessage(userId, message));
+        String text = message.getMessage();
+        if (shouldDoJokes(text)){
+            String joke = getJoke(text);
+            if (joke != null){
+                SendMessageRequest jokeMessage = new SendMessageRequest();
+                jokeMessage.setTo(message.getTo());
+                jokeMessage.setMessage(joke);
+                messagesRepository.persist(convertToMessage(userId, jokeMessage));
+            }
+        }
+    }
+
+    private boolean shouldDoJokes(String message){
+        if (message.split(" ").length > 3) {
+            double value = this.random.nextDouble();
+            return value * 100 > threshold;
+        }
+        return false;
+    }
+
+    private String getJoke(String message){
+        for (String word: Arrays.stream(message.split(" ")).sorted(Comparator.comparingInt(String::length)).toList()){
+            String joke = "";
+            // elastic.getJokeOrNull(word);
+            if (joke != null){
+                return joke;
+            }
+        };
+        return null;
     }
 
     private GetMessageRequest convertToRequest(Message message) {
